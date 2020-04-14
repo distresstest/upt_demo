d4526ff1f052c92ca3450b40610cdc736ce2397f
steven cooper <distresstest@gmail.com>
2020-04-14 09:51:15 +0100

    Working Trail

--- upt_demo/trail/views.py
+++ upt_demo/trail/views.py
@@ -1,15 +1,37 @@
 # Other packages
 from secrets import token_urlsafe
+from datetime import datetime, timezone
 
 # django packages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
 
 # My packages
 from .models import Trail, Game, Item, Location
 from .forms import TrailForm, LocationForm
+
+
+# utility functions
+def get_users_items(user):
+    pass
+
+def get_game_data(user):
+    # get game data
+    games_list = Game.objects.filter(game_player=user).order_by('-game_start')[:1]
+    print(games_list)
+    game_data = games_list[0]
+    print(game_data)
+    print('game_data.id = %s' % game_data.id)
+    print('game_data.game_player = %s' % game_data.game_player)
+    print('game_data.game_name = %s' % game_data.game_name)
+    print('game_data.game_mission = %s' % game_data.game_mission)
+    print('game_data.game_start = %s' % game_data.game_start)
+    print('game_data.game_inventory = %s' % game_data.game_inventory)
+    print('game_data.game_url = %s' % game_data.game_url)
+    print('game_data.game_total_items = %s' % game_data.game_total_items)
+
+    return games_list
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -21,12 +43,7 @@ def trail_main_view(request, id=1):
     # get trail data
     trail_data = get_object_or_404(Trail, id=id)
 
-    print('trail_data.trail_name = %s' % trail_data.trail_name)
-    print('trail_data.trail_description = %s' % trail_data.trail_description)
-    print('trail_data.trail_mission = %s' % trail_data.trail_mission)
-    print('trail_data.trail_total_items %s' % trail_data.trail_total_items)
-
-   # Check if it is a POST
+    # Check if it is a POST
     if request.method == 'POST':
         print('Hello! Button has been pressed')
         form = TrailForm(request.POST)
@@ -57,125 +74,100 @@ def trail_main_view(request, id=1):
     return render(request, "trail/trail_main.html", context)
 
 
-
-
-
 @login_required(login_url='/login/')
 def game_main_view(request): #, id):
-    print("-------------------------------")
-    print("In game_main_view...")
+    print("------------ game_main_view -----------------")
+
+    # Check if it is a POST from finish button click
+    if request.method == 'POST':
+        if request.POST.getlist('end_button')[0] == 'Finish':
+            return render(request, "game/game_finish.html", {})
+
+    # Work out which items the current player has got in their game
     current_user = request.user
-
-    # get game data
-    gameslist = Game.objects.filter(game_player=current_user).order_by('-game_start')[:1]
-    print(gameslist)
-    game_data = gameslist[0]
-    print(game_data)
-    print('game_data.id = %s' % game_data.id)
-    print('game_data.game_player = %s' % game_data.game_player)
-    print('game_data.game_name = %s' % game_data.game_name)
-    print('game_data.game_mission = %s' % game_data.game_mission)
-    print('game_data.game_start = %s' % game_data.game_start)
-    print('game_data.game_inventory = %s' % game_data.game_inventory)
-    print('game_data.game_url = %s' % game_data.game_url)
-    print('game_data.game_total_items = %s' % game_data.game_total_items)
-
-    items = Item.objects.distinct().filter(game__in=gameslist)
+    games_list = get_game_data(current_user)
+    game_data = games_list[0]
+    items = Item.objects.distinct().filter(game__in=games_list)
     print("You have the following items...")
     for item in items:
         print("> %s : %s" % (item.item_name, item.item_description))
 
+    # Check if all items have been found
     current_items = len(items)
-
-    if current_user == game_data.game_total_items:
+    if current_items == game_data.game_total_items:
         trail_completed = True
     else:
         trail_completed = False
+
+    # Update the current duration
+    now = datetime.now(timezone.utc)
+    game_duration = now - game_data.game_start
 
     # Add it all to context dictionary
     context = {
         "items": items,
         "current_items": current_items,
         "object": game_data,
+        "duration": game_duration,
         "complete": trail_completed,
         }
 
     return render(request, "game/game_main.html", context)
 
-
-
+@login_required(login_url='/login/')
 def location_detail_view(request, id):
+    print("------------ location_detail_view -----------------")
 
     # Get the location data
     location_data = get_object_or_404(Location, id=id)
 
-    # Print out the data
-    print('location_data.id = %s' % location_data.id)
-    print('location_data.location_name = %s' % location_data.location_name)
-    print('location_data.location_description = %s' % location_data.location_description)
-
     # Check if it is a POST
     if request.method == 'POST':
-        print('Hello! Button has been pressed')
+        # Work out which item has been taken
+        item_index = request.POST.getlist('item')[0]
+        item = Item.objects.filter(id=item_index)
+        print('You have picked up a %s' % item[0].item_name)
 
-        request.POST.getlist('item')
-        form = LocationForm(request.POST)
-        print(form)
-        print(request.POST)
-        for n in request.POST:
-            print(n)
+        #  Add it to game inventory
+        current_item = Item(id=item_index)
+        games_list = get_game_data(request.user)
+        current_game = Game(id=games_list[0].id)
+        current_game.game_inventory.add(current_item)
 
-        item = Item.objects.filter(id=1)
-        print('You have picked up item %s' % item[0].item_name)
+        # redirect to game_main_view
+        return HttpResponseRedirect('/game/')
 
-
-    locationlist = Location.objects.filter(id=id)
-
-    location_items = Item.objects.distinct().filter(location__in=locationlist)
+    location_list = Location.objects.filter(id=id)
+    location_items = Item.objects.distinct().filter(location__in=location_list)
     print("The location has the following items...")
     for item in location_items:
         print('> %s : "%s", "%s" (%s)' % (item.item_name, item.item_description, item.item_alt, item.item_image))
 
-    total_items = len(location_items)
+    # get current game_inventory
+    games_list = get_game_data(request.user)
+    current_game = Game(id=games_list[0].id)
+    game_items = Item.objects.distinct().filter(game__in=games_list)
+    print("You have the following items...")
+    for item in game_items:
+        print("> %s : %s" % (item.item_name, item.item_description))
 
+    location_items_left = location_items.difference(game_items)
+
+    total_items = len(location_items_left)
+
+    # Add it all to the context
     context = {
         "location_data": location_data,
-        "location_items": location_items,
+        "location_items_left": location_items_left,
         "total_items": total_items,
     }
-
-    print(context)
 
     return render(request, "locations/location_detail.html", context)
 
 
+@login_required(login_url='/login/')
+def game_finish_view(request):
+    print("------------ game_finish_view -----------------")
 
-
-
-
-
-
-
-
-
-
-#### Class Based Views Stuff -  Will look at later in more detail!
-# from .forms import TrailForm
-# from django.utils.decorators import method_decorator
-# from django.views.generic import TemplateView
-#
-# class TrailMainView(TemplateView):
-#     template_name = 'trail_main.html'
-#
-#     def get(self, request):
-#         form = TrailForm()
-#         print("hello fro the view!")
-#         print(form)
-#         return render(request, self.template_name, {'form': form})
-#
-#     # @method_decorator(login_required)
-#     # def dispatch(self, *args, **kwargs):
-#     #     return super().dispatch(*args, **kwargs)
-
-
+    return render(request, "game/game_finish.html", {})
 
